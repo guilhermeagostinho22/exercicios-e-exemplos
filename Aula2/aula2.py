bd73ab5d6ebb8deaa27bfa8afc3bb1a8ca1f9636
guilhermeagostinho22 <guilhermeagostinho77@outlook.com>
2020-08-19 15:31:27 -0300

    Enter

--- Aula2/aula2.py
+++ Aula2/aula2.py
@@ -4,9 +4,15 @@ numeroDecimal = 2.20
 palavraCaractereDupla = "Guilherme"
 palavraCaractereSimples = 'Amanda'
 verdadeiroFalso = True
-
+#Mostrar na tela os valores das variaveis
 print(numeroInteiro)
 print(numeroDecimal)
 print(palavraCaractereDupla)
 print(palavraCaractereSimples)
 print(verdadeiroFalso)
+#mostrando na tela o tipo de dado da variavel
+print(type(numeroInteiro))
+print(type(numeroDecimal))
+print(type(palavraCaractereDupla))
+print(type(palavraCaractereSimples))
+print(type(verdadeiroFalso))
