90138b7bf2d7935041e798dec95f2174c8a619e9
guilhermeagostinho22 <guilhermeagostinho77@outlook.com>
2020-08-26 15:21:32 -0300

    Enter

---
+++ Aula2/aula4.py
@@ -0,0 +1,10 @@
+numeroA = 2
+numeroB = 3
+
+#operadores aritméticos
+
+#soma
+resposta = numeroA + numeroB
+print("resposta da soma: ", resposta)
+print("resposta da soma: ", (numeroA + numeroB))
+
