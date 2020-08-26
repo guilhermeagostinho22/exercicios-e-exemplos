88b8d98d38c539c60ffa53821819010f1119c721
guilhermeagostinho22 <guilhermeagostinho77@outlook.com>
2020-08-26 15:28:35 -0300

    Enter

--- Aula2/aula4.py
+++ Aula2/aula4.py
@@ -8,3 +8,12 @@ resposta = numeroA + numeroB
 print("resposta da soma: ", resposta)
 print("resposta da soma: ", (numeroA + numeroB))
 
+#subtração
+respostaSub = numeroB - numeroA
+print("resposta da subtração: ", respostaSub)
+print("resposta da subtração: ", (numeroB - numeroA) )
+
+#multiplicação
+respostaMult = numeroA * numeroB
+print("resposta da multiplicação: ", respostaMult)
+print("resposta da multiplicação: ", (numeroA * numeroB) )
