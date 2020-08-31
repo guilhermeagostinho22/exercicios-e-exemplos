830c201cf0a4e152f7c7674d091b1e8d6faf568e
guilhermeagostinho22 <guilhermeagostinho77@outlook.com>
2020-08-31 13:52:49 -0300

    feat

--- Pyton/exercicio26.py
+++ Pyton/exercicio26.py
@@ -1,2 +1,2 @@
 metrosQuadrados = float(input("Digite o valor em metros quadrados: "))
-print("valor em hectares: ",(metrosQuadrados*0.0001))
+print("valor em hectares é: ",(metrosQuadrados*0.0001))
