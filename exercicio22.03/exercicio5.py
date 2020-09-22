3ed892f30736df8496a750d44829bbb6f11819fc
guilhermeagostinho22 <guilhermeagostinho77@outlook.com>
2020-09-22 14:06:07 -0300

    Feat

---
+++ exercicio22.03/exercicio5.py
@@ -0,0 +1,22 @@
+escolha = int(input("1-cadastrar ou 2-Não cadastrar: "))
+cadastro = 0
+
+if cadastro <= 4:
+    if escolha == 1:
+        nomeCompleto = SyntaxWarning(input("Digite seu Nome completo: ")) 
+        cpf = int(input("Digite seu CPF: "))
+        senha = int(input("Digite sua senha: "))
+        info = int(input("1-apto ou 2-Não está apto: "))
+        print("Nome completo: ",nomeCompleto)
+        print("CPF: ",cpf)
+        if info == 1:
+            print("Apto: Está apto") 
+        else: 
+            print("Apto: Não está apto")   
+        cadastro = cadastro + 1         
+    else:
+        print("Obrigado!!!")   
+else:
+    print("Obrigado!!!")          
+    
+   
