99df544a04de2da90180acd6d93a6a0be7d4d155
guilhermeagostinho22 <guilhermeagostinho77@outlook.com>
2020-09-30 14:33:24 -0300

    feat

--- infosatc-ip-avaliativo-02/exercicio6.py
+++ infosatc-ip-avaliativo-02/exercicio6.py
@@ -4,6 +4,7 @@ livros = ["harry potter","50 tons de cinza"]
 jogos = ["cs:go","gow"] 
 listaGeral = [filmes,esporte,livros,jogos]
 
+print("Lista Geral: ", listaGeral)
 print("um livro: ", livros[0])
 
 
