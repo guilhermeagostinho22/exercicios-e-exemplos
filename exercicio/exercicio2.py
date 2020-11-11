972d8f2ed7dcd01487e962f6e69b9741618c5362
guilhermeagostinho22 <guilhermeagostinho77@outlook.com>
2020-11-11 15:08:40 -0300

    fix

---
+++ exercicio/exercicio2.py
@@ -0,0 +1,4 @@
+v1 = float(input("digite um número: "))
+v2 = float(input("digite um número: "))
+f=lambda a,b: a*b
+print(f(v1,v2))
---
+++ exercicio/exercicio4.py
@@ -0,0 +1,16 @@
+n1 = int(input("digite um número: "))
+n2 = int(input("digite mais um número: "))
+n3 = int(input("digite mais um número: "))
+n4 = int(input("digite mais um número: "))
+n5 = int(input("digite mais um número: "))
+n6 = int(input("digite mais um número: "))
+n7 = int(input("digite mais um número: "))
+n8 = int(input("digite mais um número: "))
+n9 = int(input("digite mais um número: "))
+n10 = int(input("digite mais um número: "))
+lista = [n1,n2,n3,n4,n5,n6,n7,n8,n9,n10]
+
+numero=filter(lambda x: '%2==0' in x, [lista])  
+print(list(numero))
+numero2=filter(lambda x: '%!=0' in x, [lista])
+print(list(numero2))
